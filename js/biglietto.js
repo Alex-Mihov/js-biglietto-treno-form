cd24f17803cb252ac174c036e88df58a7bd2fa92
Alex Mihov <alexmihov.as@gmail.com>
2025-01-13 15:56:28 +0100

    feat elementi input ed output

--- js/biglietto.js
+++ js/biglietto.js
@@ -1,3 +1,23 @@
 // selezione elementi di input
+const nome = document.getElementById("nome")
+const chilometri = document.getElementById("km")
+const eta = document.getElementById("eta")
+const form = document.querySelector('form');
 
-// selezione elenti output
+// selezione elementi output
+const acquirente = document.getElementById("acquirente")
+const sconto = document.getElementById("sconto")
+const prezzoFinale = document.getElementById("prezzo-finale")
+
+
+// prendiamo i dati del form e li scriviamo nei blocchi
+
+form.addEventListener("submit", (event) => {
+    // Blocco l'invio del form
+    event.preventDefault();
+
+    // output acquirente
+    acquirente.innerText = nome.value.trim();
+
+    // ou
+});
